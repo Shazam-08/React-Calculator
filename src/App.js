0c8af2f44cd4496abc8b636f2b3ca0553b76eb5b
SHUBHAM KUMAR <shubhamkumar@SHUBHAMs-MacBook-Air.local>
2023-08-08 23:10:53 +0530

    some styling done

--- src/App.js
+++ src/App.js
@@ -1,91 +1,3 @@
-// import "./App.css";
-// import React, { useState } from "react";
-
-// function App() {
-//   const [result, setResult] = useState("");
-
-//   const handleClick = (e) => {
-//     setResult(result.concat(e.target.value));
-//   };
-
-//   const clear = () => {
-//     setResult("");
-//   };
-//   const backspace = () => {
-//     setResult(result.slice(0, -1));
-//   };
-//   const calculate = () => {
-//     try {
-//       setResult(eval(result).toString());
-//     } catch (err) {
-//       setResult("Error");
-//     }
-//   };
-//   return (
-//     <div className="App">
-//       <div>
-//         <div>
-//           <form>
-//             <input type="text" value={result} />
-//           </form>
-//           <div>
-//             <button onClick={clear}>Clear</button>
-//             <button onClick={backspace}>C</button>
-//             <button value="/" onClick={handleClick}>
-//               &divide;
-//             </button>
-//             <button value="7" onClick={handleClick}>
-//               7
-//             </button>
-//             <button value="8" onClick={handleClick}>
-//               8
-//             </button>
-//             <button value="9" onClick={handleClick}>
-//               9
-//             </button>
-//             <button value="*" onClick={handleClick}>
-//               &times;
-//             </button>
-//             <button value="4" onClick={handleClick}>
-//               4
-//             </button>
-//             <button value="5" onClick={handleClick}>
-//               5
-//             </button>
-//             <button value="6" onClick={handleClick}>
-//               6
-//             </button>
-//             <button value="-" onClick={handleClick}>
-//               &ndash;
-//             </button>
-//             <button value="1" onClick={handleClick}>
-//               1
-//             </button>
-//             <button value="2" onClick={handleClick}>
-//               2
-//             </button>
-//             <button value="3" onClick={handleClick}>
-//               3
-//             </button>
-//             <button value="+" onClick={handleClick}>
-//               +
-//             </button>
-//             <button value="0" onClick={handleClick}>
-//               0
-//             </button>
-//             <button value="." onClick={handleClick}>
-//               .
-//             </button>
-//             <button onClick={calculate}>=</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import "./App.css";
 import React, { useState } from "react";
 
@@ -129,15 +41,23 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div>
+    <div className="flex items-center justify-center h-screen">
+      <div className=" bg-black w-96 h-auto mx-10 px-3 border border-gray-400 rounded-md shadow-md pb-4 pt-20">
         <div>
           <form>
-            <input type="text" value={result} />
+            <input
+              type="text"
+              value={result}
+              className="w-full text-right text-white text-3xl bg-transparent border-none outline-none"
+            />
           </form>
-          <div>
-            <button onClick={clear}>Clear</button>
-            <button onClick={backspace}>C</button>
+          <div className=" grid grid-cols-3 gap-4 content-between text-3xl text-white ">
+            <button
+              className="rounded-full border border-white col-span-3 "
+              onClick={clear}
+            >
+              Clear
+            </button>
             <button value="/" onClick={handleOperatorClick}>
               ÷
             </button>
@@ -180,10 +100,20 @@ function App() {
             <button value="0" onClick={handleClick}>
               0
             </button>
+            <button
+              className="rounded-full border  border-white"
+              onClick={backspace}
+            >
+              C
+            </button>
             <button value="." onClick={handleClick}>
               .
             </button>
-            <button value="=" onClick={handleClick}>
+            <button
+              className="rounded-full border  border-white col-span-2"
+              value="="
+              onClick={handleClick}
+            >
               =
             </button>
           </div>
